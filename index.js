6e97dc1961cbf9555596e08c8c96d40f47d5bd61
catcecilia <phuongcatngo@gmail.com>
2020-10-26 03:17:28 -0400

    COMPLETED

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
-// what function does: get number from user input
-function userNumber(){
-    let numDivs = document.getElementById('numDivs').value;
-    numDivs = Number(numDivs);    
-    tableIt(numDivs);
-}
-
-
-//what function does: remove anything in #container, append a create userNumberxuserNumber grid
-function tableIt(numDivs){
-    let container = document.getElementById('container');
+const container = document.getElementById('container');
+divsNum = 16;
+size = 500;  // max size for each line
+  function makeGrid(){
     container.innerHTML = "";
-    for (i = 0; i < numDivs; i++) { //creates rows
-        let grid = document.createElement('div');
-        grid.classList.add('grid-row');
+    container.setAttribute("style", "margin: auto; width: 500px; height: 500px;");
+    for (i = 0; i<divsNum; i++){
+      const gridBox = document.createElement('div');
+      gridBox.classList.add('gridbox');
+      container.appendChild(gridBox);
+      for (j=0; j<divsNum; j++){
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
         grid.innerHTML = '&nbsp';
-        for (j = 0; j <numDivs; j++) { //creates items in the row (aka columns)
-            let gridBox = document.createElement('div');
-            gridBox.classList.add('grid-column');
-            gridBox.innerHTML = '&nbsp';
-            grid.appendChild(gridBox);
-        }
-        container.appendChild(grid);
+        grid.style.height = size/divsNum;
+        grid.style.width = size/divsNum;
+        gridBox.appendChild(grid);
+
+        //changes div to be active when moused over
+        grid.addEventListener('mouseover', function (){
+          grid.classList.add('active');
+        });
+      }
     }
- //NOTE- this currently is 'working' (runs but does not show the grid) when not called onload, has to be called on console right now 10/13/2020
+  }
+
+  makeGrid();
+
+  function clearMe(){
+    container.innerHTML= "";
+    divsNum = parseInt(prompt("How many boxes per side do you want? (min value of 2, max value of 100)"));
+    if (isNaN(divsNum) || divsNum > 100 || divsNum < 2) {
+      alert('Error! No can do. Refreshing page');
+      divsNum = 16;
+      makeGrid();
+    } else {
+      makeGrid();
+    }
+  }
+
+function changeColor(deltaColor){
+  var randomColor = Math.floor(Math.random()*16777215).toString(16);
+  const root = document.documentElement;
+  if (deltaColor == "white") {
+    root.style.setProperty('--base', 'white');
+  } else if (deltaColor == "blue") {
+    root.style.setProperty('--base', 'blue')
+  } else {
+    root.style.setProperty('--base', 'black');
+
+  }
+  
 }
-
-
-
-// upon window load, call a function with starting (post) hover black
-
-
-
-
-// upon clicking another radio button, change the (post) hover to user input decision
-
-
-
-
-// clear the color of the etch trail
-function clearMe() {}
